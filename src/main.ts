c5f5ed84da19631b8a2a6772ae951ba6db02d968
FrancoEnriquePM <francokikepariamo@gmail.com>
2025-09-22 09:58:06 -0400

    enable service worker and web manifest

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+// Registrar el Service Worker solo en el entorno de producción
+if (!isDevMode()) {
+  provideServiceWorker('ngsw-worker.js', {
+    enabled: true,
+    // La configuración para los Service Worker solo se ejecuta
+    // cuando la aplicación se estabiliza.
+    registrationStrategy: 'registerWhenStable:30000'
+  });
+}
